c66879299d7bf044dd1912181e581fc7c4435fe0
DiRoGa <100321833+DiRoGa@users.noreply.github.com>
2022-10-18 12:38:23 +0200

    Add files via upload
    Updated response messages and deleted private msg verification

--- main.js
+++ main.js
@@ -36,24 +36,18 @@ client.on('ready', () => {
 async function introBot() {
     client.on('message_create', async msg => {
         let chat = await msg.getChat();
+		
+		option = Math.floor(Math.random() * 4);
 
         if (msg.body === '!activate') {
             console.log('Activado');
             if (chat.isGroup) {
                 chat.sendMessage('_*[ACTIVANDO PROTOCOLO EVA-02]*_');
-                await sleep(2000);
-                chat.sendMessage('■■■□□□□□□□□□□□□□ 5%');
-                await sleep(2000);
-                chat.sendMessage('■■■■■■■■■□□□□□□ 60%');
-                await sleep(3000);
-                chat.sendMessage('■■■■■■■■■■■□□□□ 80%');
-                await sleep(4000);
-                chat.sendMessage('■■■■■■■■■■■■■■■■■■ 100%');
                 await sleep(3000);
                 chat.sendMessage('*[PROTOCOLO ACTIVADO]*');
-                await sleep(2000);
+                await sleep(4000);
                 chat.sendMessage('_*-Hola, ¿como estas? Soy Asu-*_');
-                await sleep(2000);
+                await sleep(3000);
                 chat.sendMessage('_*Espera, no eres Ryoji. Olvidame*_');
                 chat.sendMessage(await MessageMedia.fromUrl('https://i.gifer.com/Hhod.mp4'), { sendVideoAsGif: true });
 
@@ -74,33 +68,40 @@ async function introBot() {
 
                 console.log('El código se esta ejecutando correctamente');
 
-            } else if (chat.isGroup === false && block === false) {
-                client.sendMessage(msg.from, '_*¿Estás intentando usar esto fuera de un grupo? Baka...*_');
-                block = true;
-            } else if (chat.isGroup === false && block === true) {
-                client.sendMessage(msg.from, '_*No puedes engañarme, soy mas inteligente que tu, recuerdalo.*_');
+            } else {
+				if(option === 1) {
+					client.sendMessage(msg.from, '_*¿Estás intentando usar esto fuera de un grupo? Baka...*_');
+				} else if(option === 2) {
+					client.sendMessage(msg.from, '_*No puedes engañarme: soy más inteligente que tú, recuerdalo.*_');						
+				} else if(option === 3) {
+					client.sendMessage(msg.from, '_*Alguien esta demasiado solo...*_');						
+				}
             }
         }
 
-        if (block === false) {
-            if (msg.body.indexOf('Asuka') > -1 || msg.body.indexOf('asuka') > -1) {
-                console.log('He entrado a la condicion');
-                chat.sendMessage('_*-La piloto (y mejor piloto) del EVA-02*_');
-                chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
-            }
+		if (msg.body.indexOf('Asuka') > -1 || msg.body.indexOf('asuka') > -1) {
+			console.log('He entrado a la condicion Asuka');
+			chat.sendMessage('_*La piloto (y mejor piloto) del EVA-02*_');
+			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
+		}
 
-            if (msg.body.indexOf('Rei') > -1 || msg.body.indexOf('rei') > -1) {
-                console.log('He entrado a la condicion');
-                chat.sendMessage('_*-Dios, no me hables de esa marioneta...*_');
-                chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/Y7n1mFU.mp4'), { sendVideoAsGif: true });
-            }
+		if (msg.body.indexOf('Rei') > -1 || msg.body.indexOf('rei') > -1) {
+			console.log('He entrado a la condicion Rei');
+			chat.sendMessage('_*Dios, no me hables de esa marioneta...*_');
+			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/Y7n1mFU.mp4'), { sendVideoAsGif: true });
+		}
 
-            if (msg.body.indexOf('Shinji') > -1 || msg.body.indexOf('shinji') > -1) {
-                console.log('He entrado a la condicion');
-                chat.sendMessage('_*-Baka...*_');
-                chat.sendMessage(await MessageMedia.fromUrl('https://imgur.com/oYvIkpk.mp4'), { sendVideoAsGif: true },);
-            }
-        };
+		if (msg.body.indexOf('Shinji') > -1 || msg.body.indexOf('shinji') > -1) {
+			console.log('He entrado a la condicion Shinji');
+			chat.sendMessage('_*Baka...*_');
+			chat.sendMessage(await MessageMedia.fromUrl('https://imgur.com/oYvIkpk.mp4'), { sendVideoAsGif: true },);
+		}
+		
+		if (msg.body.indexOf('Misato') > -1 || msg.body.indexOf('misato') > -1) {
+			console.log('He entrado a la condicion Misato');
+			chat.sendMessage('_*Lider de operaciones... como si supiese lo que está haciendo*_');
+			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
+		}
     });
 };
 
