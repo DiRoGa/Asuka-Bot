684601ae5f9caefd574dfae8b2cc1b8d67fff67e
Diego <100321833+DiRoGa@users.noreply.github.com>
2023-11-15 01:37:04 +0100

    Translation made for main.js

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 var cron = require('node-cron');
 
-//const weekday = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+// const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-//const fecha = new Date();
-//let dia = weekday[fecha.getDay()];
-//let hora = fecha.getMinutes().toString();
+// const date = new Date();
+// let day = weekday[date.getDay()];
+// let minutes = date.getMinutes().toString();
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 
 const qrcode = require('qrcode-terminal');
 
@@ -23,7 +22,7 @@ const client = new Client({
     }
 })
 
-block = false;
+let block = false;
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
@@ -36,72 +35,72 @@ client.on('ready', () => {
 async function introBot() {
     client.on('message_create', async msg => {
         let chat = await msg.getChat();
-		
-		option = Math.floor(Math.random() * 4);
+
+        option = Math.floor(Math.random() * 4);
 
         if (msg.body === '!activate') {
-            console.log('Activado');
+            console.log('Activated');
             if (chat.isGroup) {
-                chat.sendMessage('_*[ACTIVANDO PROTOCOLO EVA-02]*_');
+                chat.sendMessage('_*[EVA-02 PROTOCOL ACTIVATION]*_');
                 await sleep(3000);
-                chat.sendMessage('*[PROTOCOLO ACTIVADO]*');
+                chat.sendMessage('*[PROTOCOL ACTIVATED]*');
                 await sleep(4000);
-                chat.sendMessage('_*-Hola, ¿como estas? Soy Asu-*_');
+                chat.sendMessage('_*-Hello, how are you? I am Asu-*_');
                 await sleep(3000);
-                chat.sendMessage('_*Espera, no eres Ryoji. Olvidame*_');
+                chat.sendMessage('_*Wait, you are not Ryoji. Forget about me*_');
                 chat.sendMessage(await MessageMedia.fromUrl('https://i.gifer.com/Hhod.mp4'), { sendVideoAsGif: true });
 
-                hora = Math.floor(Math.random() * 24);
+                hour = Math.floor(Math.random() * 24);
                 min = Math.floor(Math.random() * 60);
 
-                cron.schedule(min + ' ' + hora + ' * * THU', async () => {
+                cron.schedule(min + ' ' + hour + ' * * THU', async () => {
                     chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/EbdbYlU.mp4'), { sendVideoAsGif: true });
-                    chat.sendMessage('_*¡FELIZ JUEVES!*_');
-                    console.log('_*¡FELIZ JUEVES!*_');
+                    chat.sendMessage('_*HAPPY THURSDAY!*_');
+                    console.log('_*HAPPY THURSDAY!*_');
                 });
                 cron.schedule('0 0 * * MON,TUE,WED,FRI', () => {
-                    chat.sendMessage('_*-Aún no es jueves*_');
+                    chat.sendMessage('_*-It\'s not Thursday yet*_');
                 });
                 cron.schedule('0 0 * * SAT,SUN', () => {
-                    chat.sendMessage('_*-Aún no es jueves, pero al menos es fin de semana*_');
+                    chat.sendMessage('_*-It\'s not Thursday yet, but at least it\'s the weekend*_');
                 });
 
-                console.log('El código se esta ejecutando correctamente');
+                console.log('The code is executing correctly');
 
             } else {
-				if(option === 1) {
-					client.sendMessage(msg.from, '_*¿Estás intentando usar esto fuera de un grupo? Baka...*_');
-				} else if(option === 2) {
-					client.sendMessage(msg.from, '_*No puedes engañarme: soy más inteligente que tú, recuerdalo.*_');						
-				} else if(option === 3) {
-					client.sendMessage(msg.from, '_*Alguien esta demasiado solo...*_');						
-				}
+                if (option === 1) {
+                    client.sendMessage(msg.from, '_*Are you trying to use this outside of a group? Baka...*_');
+                } else if (option === 2) {
+                    client.sendMessage(msg.from, '_*You can\'t fool me: I am smarter than you, remember that.*_');
+                } else if (option === 3) {
+                    client.sendMessage(msg.from, '_*Someone is too lonely...*_');
+                }
             }
         }
 
-		if (msg.body.indexOf('Asuka') > -1 || msg.body.indexOf('asuka') > -1) {
-			console.log('He entrado a la condicion Asuka');
-			chat.sendMessage('_*La piloto (y mejor piloto) del EVA-02*_');
-			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
-		}
+        if (msg.body.indexOf('Asuka') > -1 || msg.body.indexOf('asuka') > -1) {
+            console.log('Entered Asuka condition');
+            chat.sendMessage('_*The pilot (and better pilot) of EVA-02*_');
+            chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
+        }
 
-		if (msg.body.indexOf('Rei') > -1 || msg.body.indexOf('rei') > -1) {
-			console.log('He entrado a la condicion Rei');
-			chat.sendMessage('_*Dios, no me hables de esa marioneta...*_');
-			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/Y7n1mFU.mp4'), { sendVideoAsGif: true });
-		}
+        if (msg.body.indexOf('Rei') > -1 || msg.body.indexOf('rei') > -1) {
+            console.log('Entered Rei condition');
+            chat.sendMessage('_*God, don\'t talk to me about that puppet...*_');
+            chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/Y7n1mFU.mp4'), { sendVideoAsGif: true });
+        }
 
-		if (msg.body.indexOf('Shinji') > -1 || msg.body.indexOf('shinji') > -1) {
-			console.log('He entrado a la condicion Shinji');
-			chat.sendMessage('_*Baka...*_');
-			chat.sendMessage(await MessageMedia.fromUrl('https://imgur.com/oYvIkpk.mp4'), { sendVideoAsGif: true },);
-		}
-		
-		if (msg.body.indexOf('Misato') > -1 || msg.body.indexOf('misato') > -1) {
-			console.log('He entrado a la condicion Misato');
-			chat.sendMessage('_*Lider de operaciones... como si supiese lo que está haciendo*_');
-			chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
-		}
+        if (msg.body.indexOf('Shinji') > -1 || msg.body.indexOf('shinji') > -1) {
+            console.log('Entered Shinji condition');
+            chat.sendMessage('_*Baka...*_');
+            chat.sendMessage(await MessageMedia.fromUrl('https://imgur.com/oYvIkpk.mp4'), { sendVideoAsGif: true });
+        }
+
+        if (msg.body.indexOf('Misato') > -1 || msg.body.indexOf('misato') > -1) {
+            console.log('Entered Misato condition');
+            chat.sendMessage('_*Operations leader... as if she knows what she\'s doing*_');
+            chat.sendMessage(await MessageMedia.fromUrl('https://i.imgur.com/vXyRjBZ.mp4'), { sendVideoAsGif: true });
+        }
     });
 };
 
